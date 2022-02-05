aa25ee2d0f0d9c229361fbdbd5ddc249a2a8f509
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-05 18:55:42 +0330

    update offline mode - indexedDB

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCache = "site-static-v2"; //static version
-const dynamicCache = "site-dynamic-v1"; //dynamic version
+const staticCache = "site-static-v6"; //static version
+const dynamicCache = "site-dynamic-v8"; //dynamic version
 const assets = [
   "/",
   "/index.html",
@@ -50,25 +50,27 @@ self.addEventListener("activate", (evt) => {
 
 //fetch
 self.addEventListener("fetch", (evt) => {
-  evt.respondWith(
-    caches
-      .match(evt.request)
-      .then((cacheRes) => {
-        return (
-          cacheRes ||
-          fetch(evt.request).then((fetchRes) => {
-            return caches.open(dynamicCache).then((cache) => {
-              cache.put(evt.request.url, fetchRes.clone());
-              limitCacheSize(dynamicCache, 15);
-              return fetchRes;
-            });
-          })
-        );
-      })
-      .catch(() => {
-        if (evt.request.url.indexOf(".html") > -1) {
-          return caches.match("/pages/fallback.html");
-        }
-      })
-  );
+  if (evt.request.url.indexOf("firestore.googleapi.com") === -1) {
+    evt.respondWith(
+      caches
+        .match(evt.request)
+        .then((cacheRes) => {
+          return (
+            cacheRes ||
+            fetch(evt.request).then((fetchRes) => {
+              return caches.open(dynamicCache).then((cache) => {
+                cache.put(evt.request.url, fetchRes.clone());
+                limitCacheSize(dynamicCache, 15);
+                return fetchRes;
+              });
+            })
+          );
+        })
+        .catch(() => {
+          if (evt.request.url.indexOf(".html") > -1) {
+            return caches.match("/pages/fallback.html");
+          }
+        })
+    );
+  }
 });
